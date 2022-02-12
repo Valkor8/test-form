e3f7cf7243c4d444410864af793495b324ad87a3
DrZelim <drzelim@mail.ru>
2022-02-12 16:03:31 +0300

    Тестовое задание на позицию верстальщика

---
+++ gulpfile.js
@@ -0,0 +1,185 @@
+"use strict";
+
+const gulp = require("gulp");
+const plumber = require("gulp-plumber");
+const sourcemap = require("gulp-sourcemaps");
+const sass = require('gulp-sass');
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
+const csso = require("postcss-csso");
+const terser = require('gulp-terser');
+const htmlmin = require("gulp-htmlmin");
+const rename = require("gulp-rename");
+const imagemin = require("gulp-imagemin");
+const webp = require("gulp-webp");
+const svgstore = require("gulp-svgstore");
+const del = require("del");
+const sync = require("browser-sync").create();
+
+// Styles
+
+const styles = () => {
+    return gulp.src("source/sass/style.scss")
+        .pipe(plumber())
+        .pipe(sourcemap.init())
+        .pipe(sass())
+        .pipe(gulp.dest("source/css"))
+        .pipe(postcss([
+            autoprefixer(),
+            csso()
+        ]))
+        .pipe(rename("style.min.css"))
+        .pipe(sourcemap.write("."))
+        .pipe(gulp.dest("build/css"))
+        .pipe(sync.stream());
+}
+
+exports.styles = styles;
+
+const stylesNormalize = () => {
+  return gulp.src("source/css/normalize/normalize.css")
+      .pipe(gulp.dest("source/css/normalize"))
+      .pipe(postcss([
+          autoprefixer(),
+          csso()
+      ]))
+      .pipe(rename("normalize.min.css"))
+      .pipe(gulp.dest("build/css"))
+      .pipe(sync.stream());
+}
+
+exports.stylesNormalize = stylesNormalize;
+
+// HTML
+const html = () => {
+    return gulp.src("source/*.html")
+        .pipe(htmlmin({collapseWhitespace: true}))
+        .pipe(gulp.dest("build"))
+}
+
+// Scripts
+
+const scripts = () => {
+    return gulp.src("source/js/*.js")
+        .pipe(terser())
+        .pipe(gulp.dest("build/js"))
+        .pipe(sync.stream());
+}
+
+exports.scripts = scripts;
+
+// Images
+
+const images = () => {
+    return gulp.src("source/img/**/*.{jpg,png,svg}")
+        .pipe(imagemin([
+            imagemin.mozjpeg({progressive: true}),
+            imagemin.optipng({optimizationLevel: 3}),
+            imagemin.svgo()
+        ]))
+        .pipe(gulp.dest("build/img"));
+}
+
+exports.images = images;
+
+// Webp
+
+const createWebp = () => {
+    return gulp.src("source/img/**/*.{jpg,png}")
+        .pipe(webp({quality: 90}))
+        .pipe(gulp.dest("build/img"))
+}
+
+exports.createWebp = createWebp;
+
+// Sprite
+
+const sprite = () => {
+    return gulp.src("source/img/icons/*.svg")
+        .pipe(svgstore())
+        .pipe(rename("sprite.svg"))
+        .pipe(gulp.dest("build/img"));
+}
+
+exports.sprite = sprite;
+
+// Copy
+
+const copy = () => {
+    return gulp.src([
+        "source/fonts/*.{woff2,woff}",
+        "source/*.ico",
+        "source/img/**/*.{jpg,png,svg}"
+    ],
+        {
+            base: "source"
+        })
+        .pipe(gulp.dest("build"));
+}
+
+exports.copy = copy;
+
+// Clean
+
+const clean = () => {
+    return del ("build");
+}
+
+exports.clean = clean;
+
+//  Server
+
+const server = (done) => {
+    sync.init({
+        server: {
+            baseDir: "build"
+        },
+        notify: false,
+        cors: true,
+        ui: false
+    });
+  done();
+}
+
+exports.styles = styles;
+
+// Watcher
+
+const watcher = () => {
+    gulp.watch("source/sass/**/*.scss", gulp.series("styles"));
+    gulp.watch("source/js/*.js", gulp.series(scripts)); // Из-за неработающего uglify
+    gulp.watch("source/*.html", gulp.series(html)).on("change", sync.reload);
+}
+
+// Build
+const build = gulp.series(
+    clean,
+    gulp.parallel(
+        styles,
+        stylesNormalize,
+        html,
+        sprite,
+        copy,
+        images,
+        scripts,
+        createWebp,
+        images
+    )
+)
+
+exports.build = build;
+
+exports.default = gulp.series(
+    clean,
+    gulp.parallel(
+        styles,
+        stylesNormalize,
+        sprite,
+        html,
+        copy,
+        scripts
+    ),
+    gulp.series(
+        server, watcher
+    )
+)
---
+++ source/js/main.js
@@ -0,0 +1,203 @@
+'use stirct';
+
+const form = document.querySelector('#form');
+const inn = form.querySelector('.inn');
+const innError = form.querySelector('#inn-error');
+const checkbox = form.querySelector('.form__checkbox-indicator');
+const residence = form.querySelector('.residence');
+const radioResidence = Array.from(form.querySelectorAll('.form__radio-residence .form__radio-indicator'));
+const residenceError = form.querySelector('#residenceError');
+const radioPublic = Array.from(form.querySelectorAll('.public .form__radio-indicator'));
+const formButton = form.querySelector('.form__continue-btn');
+const formMomey = form.querySelector('.form__money');
+const moneyError = form.querySelector('#moneyError');
+const residenceInvalid = form.querySelector('#residence-error');
+const yesNoError = form.querySelector('.form__yes-no-error');
+const yesNoError2 = form.querySelector('.form__yes-no-error-2');
+
+const LENGTH = 12;
+const MAX_LENGTH = 12;
+
+const innValidity = () => {
+  innError.style.display='none';
+  if (inn.value.length !== LENGTH) {
+    inn.setCustomValidity(`Значение поля ИНН должно быть равно ${LENGTH} символам`);
+  } else if (inn.validity.valueMissing) {
+    inn.setCustomValidity('Поле обязательно для заполнения');
+  } else {
+    inn.setCustomValidity('');
+  }
+
+  inn.reportValidity();
+}
+
+inn.addEventListener('input', innValidity);
+
+const checkboxClickHandler = () => {
+  if (checkbox.checked) {
+    inn.value = '';
+    inn.setAttribute('disabled', 'disabled');
+    inn.removeEventListener('input', innValidity);
+    innError.style.display='none';
+  } else {
+    inn.removeAttribute('disabled');
+    inn.addEventListener('input', innValidity);
+  }
+};
+
+checkbox.addEventListener('change', checkboxClickHandler);
+
+const innDisabled = () => {
+  inn.value = '';
+  inn.setAttribute('disabled', 'disabled');
+  inn.removeEventListener('input', innValidity);
+  checkbox.removeEventListener('change', checkboxClickHandler);
+  checkbox.setAttribute('disabled', 'disabled');
+  residenceInvalid.style.display='inline';
+}
+
+const innEnabled = () => {
+  if (!checkbox.checked) {
+    inn.removeAttribute('disabled');
+    inn.addEventListener('input', innValidity);
+  }
+  checkbox.addEventListener('change', checkboxClickHandler);
+  checkbox.removeAttribute('disabled');
+  residenceInvalid.style.display='none';
+}
+
+// residence.addEventListener('change', (evt) => {
+//   if (evt.target.value === 'USA') {
+//     innDisabled();
+//     radioResidence.forEach((item) => {
+//       if (item.dataset.id === 'yes') {
+//         item.setAttribute('checked', true)
+//       } else {
+//         item.removeAttribute('checked');
+//       }
+//     });
+//   } else {
+//     innEnabled();
+//     radioResidence.forEach((item) => item.removeAttribute('checked'));
+//   }
+// });
+
+radioResidence.forEach((item) => {
+  item.addEventListener('click', (evt) => {
+    if (evt.target.dataset.id === 'yes') {
+      innDisabled();
+      residence.value = 'USA';
+      yesNoError.style.display="none";
+      residenceError.style.display="none";
+      innError.style.display='none';
+    } else {
+      innEnabled();
+      if (residence.value === 'USA') {
+        residence.value = '';
+      }
+      yesNoError.style.display="none";
+    }
+  });
+});
+
+radioPublic.forEach((item) => {
+  item.addEventListener('click', (evt) => {
+    if (evt.target.dataset.id === 'yes' || evt.target.dataset.id === 'no') {
+      yesNoError2.style.display="none";
+    }
+  });
+});
+
+const isRadioResidenceValid = () => {
+  let isValid = false;
+  radioResidence.forEach((item) => {
+    if (item.checked) {
+      isValid = true;
+      yesNoError.style.display="none";
+    }
+  });
+  if (!isValid) {
+    yesNoError.style.display="inline";
+  }
+  return isValid
+}
+
+const isRadioPublicValid = () => {
+  let isValid = false;
+  radioPublic.forEach((item) => {
+    if (item.checked) {
+      isValid = true;
+    }
+  });
+  if (!isValid) {
+    yesNoError2.style.display="inline";
+  }
+  return isValid
+}
+
+const isInnValid = () => {
+  if (checkbox.checked) {
+    return;
+  }
+  if (inn.value === '') {
+    innError.textContent = `Поле обязательно для заполнения`
+    innError.style.display='inline';
+  } else if (inn.value < 100000000000 || inn.value > 999999999999) {
+    innError.textContent = `ИНН должен состоять из 12 символов`
+    innError.style.display='inline';
+  }
+}
+
+const isResidenceValid = () => {
+  if (residence.value === '') {
+    residenceError.style.display="inline";
+  } else {
+    residenceError.style.display="none";
+  }
+};
+
+const isMoneyValid = () => {
+  if (formMomey.value === '') {
+    moneyError.style.display="inline";
+  } else {
+    moneyError.style.display="none";
+  }
+};
+
+residence.addEventListener('change', (evt) => {
+  residenceError.style.display="none";
+
+  if (evt.target.value === 'USA') {
+    innDisabled();
+    radioResidence.forEach((item) => item.dataset.id === 'yes'? item.checked = true : item.checked = false);
+  } else {
+    innEnabled();
+    radioResidence.forEach((item) => item.checked = false);
+  }
+});
+
+formMomey.addEventListener('change', () => {
+  moneyError.style.display="none";
+});
+
+formButton.addEventListener('click', (evt) => {
+  evt.preventDefault();
+
+  isInnValid();
+
+  isRadioResidenceValid();
+
+  isResidenceValid();
+
+  isMoneyValid();
+
+  if (!isRadioPublicValid()) {
+    return
+  }
+
+  if (inn.validity.valid === false || residence.validity.valid === false || formMomey.validity.valid === false) {
+    return
+  }
+
+  form.submit();
+});
